0e71076aa4f84cdfff71375f8ae6b592f1c22ac6
ncalliger <n.calliger@gmail.com>
2017-08-07 16:05:40 -0700

    Task Master list
    Added task master list array.
    Rearranged functions to make creating task boxes easier.

---
+++ platforms/browser/www/data.js
@@ -0,0 +1,26 @@
+var employees = [
+  "Arty",
+  "Tom",
+  "Noah",
+  "Steve",
+  "Pajeet"
+];
+
+var MasterTaskList = [
+  "Slice Bell Peppers",
+  "Chop Onions",
+  "Cut Onions",
+  "Cook Sausage",
+  "Shred Sausage",
+  "Cut Jalapeneos",
+  "Cut Tomatoes",
+  "Grill Garlic",
+  "Pizza Garlic",
+  "Refill Pineapples",
+  "Refill Olives",
+  "Mushrooms",
+  "Chop Steak",
+  "Small Boxes",
+  "Medium Boxes",
+  "Large Boxes"
+]
--- platforms/browser/www/methods.js
+++ platforms/browser/www/methods.js
@@ -1,24 +1,20 @@
-function populatePrepNeeded(numberOfItems){
+//this function is meant to indicate unfinished tasks.
+function populatePrepNeeded(numberOfItems, emp){
   for(var i = 0; i < numberOfItems; i++){
-  document.write(
-  "<ul>" +
-    "<li>" + "<table>" +
-      "<tr>" +
-        "<th>" + "Task" + "</th>" +
-        "<th>" + "Assigned To" + "</th>" +
-      "</tr>" +
-      "<tr>" +
-        "<td>" + "Small Boxes" + "</td>" +
-        "<td>" + "Arty" + "</td>" +
-      "</tr>" +
-    "</table>" + "</li>" +
-  "</ul>"
-  );
+    createTaskBox(emp);
   }
 }
 
-function populatePrepFinished(numberOfItems){
+
+//this function is meant to indicate finished tasks.
+function populatePrepFinished(numberOfItems, emp){
   for(var i = 0; i < numberOfItems; i++){
+    createTaskBox(emp);
+  }
+}
+
+//function for creating individual task boxes.
+function createTaskBox(emp) {
   document.write(
   "<ul>" +
     "<li>" + "<table>" +
@@ -28,10 +24,9 @@ function populatePrepFinished(numberOfItems){
       "</tr>" +
       "<tr>" +
         "<td>" + "Small Boxes" + "</td>" +
-        "<td>" + "Arty" + "</td>" +
+        "<td>" + emp + "</td>" +
       "</tr>" +
     "</table>" + "</li>" +
   "</ul>"
   );
-  }
 }
