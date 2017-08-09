3966c330bd708723eb95f54ef92e763fe48be9f4
ncalliger <n.calliger@gmail.com>
2017-08-09 14:46:06 -0700

    checlists are now fed into arrays!

--- platforms/browser/www/methods.js
+++ platforms/browser/www/methods.js
@@ -1,7 +1,7 @@
 //this function is meant to indicate unfinished tasks.
 function populatePrepNeeded(numberOfItems, emp){
   for(var i = 0; i < numberOfItems; i++){
-    createTaskBox(emp);
+    createTaskBox(unfinishedTasks[i], emp);
   }
 }
 
@@ -9,13 +9,13 @@ function populatePrepNeeded(numberOfItems, emp){
 //this function is meant to indicate finished tasks.
 function populatePrepFinished(numberOfItems, emp){
   for(var i = 0; i < numberOfItems; i++){
-    createTaskBox(emp);
+    createTaskBox(finishedTasks[i], emp);
   }
 }
 
 //function for creating individual task boxes.
 //This looks very dirty, will clean it up later.
-function createTaskBox(emp) {
+function createTaskBox(task, emp) {
   document.write(
   "<ul>" +
     "<li>" + "<table>" +
@@ -24,7 +24,7 @@ function createTaskBox(emp) {
         "<th>" + "Assigned To" + "</th>" +
       "</tr>" +
       "<tr>" +
-        "<td>" + "Small Boxes" + "</td>" +
+        "<td>" + task + "</td>" +
         "<td>" + emp + "</td>" +
       "</tr>" +
     "</table>" + "</li>" +
@@ -33,14 +33,54 @@ function createTaskBox(emp) {
 }
 
 //This function creates the entirety of the checklist.
-function createCheckList(taskArray){
-  taskArray.sort();
-  for(var i = 0;i < taskArray.length; i++){
-  //This snippet creates an individual task in the checklist.
+function createCheckList(inputArray){
+  inputArray.sort();
+  for(var i = 0;i < inputArray.length; i++){
+  //This snippet creates an individual task in the checklist. remove "checked" VVV to default to unchecked boxes
     document.write(
-      "<label><input type=\"checkbox\" name=\"task\" value=\"Large Boxes\">" +
-      taskArray[i] +
+      "<label><input type=\"checkbox\" name=\"task\" value=\"" + inputArray[i] + "\" checked>" +
+      inputArray[i] +
       "</label>"
     )
   }
+}
+
+//This function was copied from a post on StackOverflow.com
+// Returns an array with values of the selected (checked) checkboxes in "frm"
+function getSelectedChbox(frm) {
+  var selchbox = [];        // array that will store the value of selected checkboxes
+
+  // gets all the input tags in frm, and their number
+  var inpfields = frm.getElementsByTagName('input');
+  var nr_inpfields = inpfields.length;
+
+  // traverse the inpfields elements, and adds the value of selected (checked) checkbox in selchbox
+  for(var i=0; i<nr_inpfields; i++) {
+    if(inpfields[i].type == 'checkbox' && inpfields[i].checked == true) selchbox.push(inpfields[i].value);
+  }
+
+  return selchbox;
+}
+
+// Returns an array with values of the unselected (unchecked) checkboxes in "frm"
+function getUnselectedChbox(frm) {
+  var selchbox = [];        // array that will store the value of selected checkboxes
+
+  // gets all the input tags in frm, and their number
+  var inpfields = frm.getElementsByTagName('input');
+  var nr_inpfields = inpfields.length;
+
+  // traverse the inpfields elements, and adds the value of selected (checked) checkbox in selchbox
+  for(var i=0; i<nr_inpfields; i++) {
+    if(inpfields[i].type == 'checkbox' && inpfields[i].checked == false) selchbox.push(inpfields[i].value);
+  }
+
+  return selchbox;
+}
+
+//this function was also copied from StackOverflow.com
+//this function updates a specified div's content
+function updateDiv(div)
+{
+    $( "#" + div ).load(window.location.href + "#" + div );
 }
